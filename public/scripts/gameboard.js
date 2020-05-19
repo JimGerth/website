175ad3c8adb2077fbad3eb84190d3368d3012c2c
JimGerth <jim.frederic.gerth@gmail.com>
2020-05-19 13:10:58 +0200

    changed gameboard.setup to only put cells in every other spot (based on the size) so cells dont overlap anymore. however i still need to get rid of some of the cells in the corner of the grid

--- public/scripts/gameboard.js
+++ public/scripts/gameboard.js
@@ -4,10 +4,7 @@ class Gameboard {
     this.scale = scale;
     this.xoffset = scale * 1.5;
     this.yoffset = scale * 0.86602540378;
-    this.cells = [];
-    for (let y = 0; y < this.rows; y++) {
-      this.cells[y] = [];
-    }
+    this.cells = new Array(this.rows).fill(new Array(this.cols).fill(null));
     this.setup();
   }
 
@@ -15,39 +12,57 @@ class Gameboard {
   get cols() { return 2 * this.size - 1; }
 
   setup() {
-    // setup cell objects and add them to two dimensional cell array
-    // in the right way
+    // initializing two dimensional cell aray 
     let id = 0;
-    for (let x = 0; x < this.cols; x++) {
-      for (let y = 0; y < this.rows; y++) {
-        // looping through all possible cell positions
-        // find out in which position to actually put a cell
-        // if some stuff
-        // this.cells[y][x] = new Cell(
-        //   id,
-        //   1,
-        //   x * 1.5 * this.scale + this.scale,
-        //   y * 0.86602540378 * this.scale + 0.86602540378 * this.scale,
-        //   this.scale
-        // );
-        id++;
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (
+          (this.size % 2 == 0 && x % 2 != y % 2) ||
+          (this.size % 2 != 0 && x % 2 == y % 2)
+        ) {
+          this.cells[y][x] = new Cell(
+            id,
+            1,
+            x * 1.5 * this.scale + this.scale,
+            y * 0.86602540378 * this.scale + 0.86602540378 * this.scale,
+            this.scale
+          );
+          id++;
+        }
       }
     }
     // adding neighbours for all cells
-    for (let x = 0; x < this.cols; x++) {
-      for (let y = 0; y < this.rows; y++) {
-        this.cells[y][x].addNeighbour(direction.top, this.cells[y - 2][x]);
-        this.cells[y][x].addNeighbour(direction.topright, this.cells[y - 1][x + 1]);
-        this.cells[y][x].addNeighbour(direction.bottomright, this.cells[y + 1][x + 1]);
-        this.cells[y][x].addNeighbour(direction.bottom, this.cells[y + 2][x]);
-        this.cells[y][x].addNeighbour(direction.bottomleft, this.cells[y + 1][x - 1]);
-        this.cells[y][x].addNeighbour(direction.topleft, this.cells[y - 1][x - 1]);
-      }
-    }
+    // for (let x = 0; x < this.cols; x++) {
+    //   for (let y = 0; y < this.rows; y++) {
+    //     console.log(x);
+    //     console.log(y);
+    //     console.log(this.cells[y][x]);
+    //     if (this.cells[y][x]){
+    //       if (this.cells[y - 2][x]) {
+    //         this.cells[y][x].addNeighbour(direction.top, this.cells[y - 2][x]);
+    //       }
+    //       if (this.cells[y - 1][x + 1]) {
+    //         this.cells[y][x].addNeighbour(direction.topright, this.cells[y - 1][x + 1]);
+    //       }
+    //       if (this.cells[y + 1][x + 1]) {
+    //         this.cells[y][x].addNeighbour(direction.bottomright, this.cells[y + 1][x + 1]);
+    //       }
+    //       if (this.cells[y + 2][x]) {
+    //         this.cells[y][x].addNeighbour(direction.bottom, this.cells[y + 2][x]);
+    //       }
+    //       if (this.cells[y + 1][x - 1]) {
+    //         this.cells[y][x].addNeighbour(direction.bottomleft, this.cells[y + 1][x - 1]);
+    //       }
+    //       if (this.cells[y - 1][x - 1]) {
+    //         this.cells[y][x].addNeighbour(direction.topleft, this.cells[y - 1][x - 1]);
+    //       }
+    //     }
+    //   }
+    // }
     // reparse body to make the new svg elements actually show up
     $(`body`).html($(`body`).html());
     // connect callbacks
-    this.connect();
+    // this.connect();
   }
 
   connect() {
